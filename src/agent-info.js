bf738b147cc95595f18921701bab005c529d76c5
atusu <alisiatusu@gmail.com>
2023-03-06 22:14:46 +0200

    working images

--- src/agent-info.js
+++ src/agent-info.js
@@ -1,8 +1,15 @@
 const info = {
-    1234: {"url":"images/black-cat.jpg", "name":"Vali", "secret":"Secretly wants to play with mice and not eat them", "text":'Looks cute, might attack your legs later', "achievements":'This secret agent has successfully completed 435 missions and conquered everyone with his leadership skills. This has brought him "The Best Secret Agent of the year" award. \n\nDoesn\'t understand why Silviu avoids him.'},
-    2345: {"url":"images/orange-cat.jpg", "name":"Silviu", "secret":"Secretly enjoys itchy tasty but doesn't wanna admit", "text":"Looks stiff... is stiff. Will scratch couch if disturbed from cat business.", "achievements":'This secret agent has successfully completed 882 missions and received the "The Best Stolen Secret" award and won the prize for the "Best Secret Agent of the year" twice. Very competitive spirit.\n\nLost to Vali this year, still believes it\'s a joke.'},
-    3456: {"url":"images/white-cat.jpg", "name":"Catalin","secret":"Used the innocent look to blame the dog for the broken pot", "text":"Pretends he doesn't enjoy pliculetz to fool the enemies","achievements":"This secret agent has successfully completed 538 missions and received no award because believes that prizez and the mere concept of competition is just a construct meant to encourage individual performance and not the wellbeing of the whole community.."}
+    1234: {"url":"./images/black-cat.jpg", "name":"Vali", "secret":"Secretly wants to play with mice and not eat them", "text":'Looks cute, might attack your legs later', "achievements":'This secret agent has successfully completed 435 missions and conquered everyone with his leadership skills. This has brought him "The Best Secret Agent of the year" award. \n\nDoesn\'t understand why Silviu avoids him.'},
+    2345: {"url":"./images/orange-cat.jpg", "name":"Silviu", "secret":"Secretly enjoys itchy tasty but doesn't wanna admit", "text":"Looks stiff... is stiff. Will scratch couch if disturbed from cat business.", "achievements":'This secret agent has successfully completed 882 missions and received the "The Best Stolen Secret" award and won the prize for the "Best Secret Agent of the year" twice. Very competitive spirit.\n\nLost to Vali this year, still believes it\'s a joke.'},
+    3456: {"url":"./images/white-cat.jpg", "name":"Catalin","secret":"Used the innocent look to blame the dog for the broken pot", "text":"Pretends he doesn't enjoy pliculetz to fool the enemies","achievements":"This secret agent has successfully completed 538 missions and received no award because believes that prizez and the mere concept of competition is just a construct meant to encourage individual performance and not the wellbeing of the whole community.."}
 };
+
+export let images={}
+
+for (var i=0; i<3;i++){
+    const value = info[Object.keys(info)[i]]
+    images[value.name] = require(`./images/${value.name}.jpg`)
+}
 
 export function getAgentInfo(id){
     if(id==null){
--- src/components/agent-profile.jsx
+++ src/components/agent-profile.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import {useParams} from "react-router-dom";
+import {getAgentInfo, images} from '../agent-info.js'
+
 
 const AgentProfile = () => {
     let {id} = useParams();
-
+    let info = getAgentInfo(id)
+    // const silviu = require('../images/orange-cat.jpg')
+    
   return (
-    <div>
-        <h1>This is the agent profile ++ {id}</h1>
+    <div id="agent-info">
+      <h1>Secret agent identity revealed</h1>
+      <p>The ultra secret info below:</p>
+
+      <div className="agent-page" id={id}>
+        <p><b>Name: </b>{info.name}</p>
+        <p><b>Image: </b></p>
+        <img src={images[info.name]}></img>
+        <p><b>Description: </b>{info.text}</p>
+        <p><b>Achievements: </b> {info.achievements}</p>
+        <p><b>Secret: </b>{info.secret}</p>
+        <p><b>ID: </b>{id}</p>
+      </div>
     </div>
   )
 }

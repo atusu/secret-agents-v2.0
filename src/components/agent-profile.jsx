670605604573e309f62f77ddbb76a37133e2984e
atusu <alisiatusu@gmail.com>
2023-03-07 23:31:03 +0200

    now the component will take the info from the local storage instead of the main source

--- src/components/agent-profile.jsx
+++ src/components/agent-profile.jsx
@@ -3,11 +3,23 @@ import {useParams} from "react-router-dom";
 import {getAgentInfo, images} from '../agent-info.js'
 
 
-const AgentProfile = () => {
-    let {id} = useParams();
-    let info = getAgentInfo(id)
-    // const silviu = require('../images/orange-cat.jpg')
-    
+const AgentProfile = () => {    
+  //get agent id from params
+  let {id} = useParams();
+
+  //check if the agent data already exists in the local storage
+  //in case it's not, we add the data to the local storage
+  if(!localStorage.getItem(id)){
+    const agentInfo = getAgentInfo(id);
+    console.log(`get agent info:: ${JSON.stringify(agentInfo)}`);
+    window.localStorage.setItem(id, JSON.stringify(agentInfo));
+  }
+
+  //in case it exists, we get that data from the local storage, 
+  //so we don't need to access the data source again
+  const info = JSON.parse(localStorage.getItem(id))
+  
+  //below we insert the component based on the info in the local storage
   return (
     <div id="agent-info">
       <h1>Secret agent identity revealed</h1>
@@ -21,6 +33,8 @@ const AgentProfile = () => {
         <p><b>Achievements: </b> {info.achievements}</p>
         <p><b>Secret: </b>{info.secret}</p>
         <p><b>ID: </b>{id}</p>
+        <br/>
+        <a href="/">Inapoi</a>
       </div>
     </div>
   )
